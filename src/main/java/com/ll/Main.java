7898608c70372b709ffdfcea44997f258fc3e3d2
silvercho <130129061+silvercho@users.noreply.github.com>
2023-11-01 17:26:20 +0900

    E/1 (#2)
    * 작업
    
    * 자작업2

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,5 +3,7 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("11월");
+        System.out.println("1일");
     }
 }
